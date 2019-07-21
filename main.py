77be993fd4514dda89f5b914310e54a7a53a008a
Lucius Hu <hsyhzj18@outlook.com>
2019-07-22 00:50:38 +0800

    07222019 Draw octagon in image
    Signed-off-by: Lucius Hu <hsyhzj18@outlook.com>

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import cv2
 import tools
 import matplotlib.pyplot as plt
+import math
+import numpy as np
 
-_img_Name = 'img/2.png'
-
+_img_Name = 'img/5.png'
+_img1 = cv2.imread(_img_Name)
 _img = cv2.imread(_img_Name)
 
 _h, _w, _d = _img.shape
 
 _img_Grey = cv2.cvtColor(_img, cv2.COLOR_BGR2GRAY)
-_thre, _bina = cv2.threshold(_img_Grey, 140, 255, cv2.THRESH_BINARY)
+_thre, _bina = cv2.threshold(_img_Grey, 100, 255, cv2.THRESH_BINARY)
 # _thre, _bina = cv2.threshold(_img_Grey[int(_h/2-100):int(_h/2+100), int(_w/2-100):int(_w/2+100)], 110, 255, cv2.THRESH_BINARY)
 # _canny = cv2.Canny(_img_Grey, 100, 255)
 
 _cnts = cv2.findContours(_bina, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2]
+cv2.drawContours(_img, _cnts, -1, [0,0,255])
 
-_MaxArea = max(_cnts, key=cv2.contourArea)      # the biggest area coords
+# the biggest area coords
 
+_MaxArea = max(_cnts, key=cv2.contourArea)
 ((x, y), _radius) = cv2.minEnclosingCircle(_MaxArea)
-_center = (int(x)-1, int(y)+2)
+_center = (math.ceil(x)-3, math.ceil(y))
 
-cv2.circle(_img, _center, 43, (0,0,255))
+# ROI in Arrow effect
 
-_biggestIndex = tools.findBiggestIndex(_cnts, 2) # the biggest area index
+ROI_CenterOfArrow = tools.ROI(_img, _center, _radius, 1)
+_ROI_cnts = cv2.findContours(ROI_CenterOfArrow, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2]
+_q1 = cv2.cvtColor(ROI_CenterOfArrow, cv2.COLOR_GRAY2RGB)
+
+# Draw ROI area
+
+cv2.drawContours(_img, _ROI_cnts, 1, [0,0,255])
+# cv2.circle(_img, _center, 41, (0,0,255))
+
+oct_Poly = tools.drawOctagon(_img, _center, _radius, 0.18, 22.5)   # Draw Octagon into image
+cv2.imshow('oct_Poly',oct_Poly)
+
+# the biggest area index
+
+# _biggestIndex = tools.findBiggestIndex(_cnts, 2)
 # print(_cnt[_biggestIndex].reshape(3027,2))
-# cv2.drawContours(_img, _cnts, 18, [0,0,255])
+cv2.drawContours(_img, _cnts, 18, [0,0,255])
 
-cv2.imshow('_img',_img)
-# cv2.imshow('_canny',_canny)
-cv2.imshow('_bina',_bina)
+
+cv2.imshow('_img1',_img1)
 
 _img = cv2.cvtColor(_img, cv2.COLOR_RGB2BGR)
 plt.imshow(_img)
@@ -36,3 +53,5 @@ plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Nest step, calcul how much pixel mathed between centre-edge and Octgaton, and then shfit and rotate to figure out the best position for Octgaton
--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 import cv2
+import numpy as np
+import math
 
-def findBiggestIndex(cnt, num):
+def findBiggestIndex(cnt, num):                                 # Find out biggest or secend area's index or area in cnts
     biggestArea = 0
     biggestIndex = 0
     secondArea = 0
@@ -16,3 +18,37 @@ def findBiggestIndex(cnt, num):
         return biggestIndex
     if num == 2:
         return secondIndex
+
+def ROI(img, center, radius, num):                              # 1 means ROI of center of Arrows
+    temp = np.ones(img.shape[:2], np.uint8) * 0
+    if num is 1:
+        print(type(center[0]))
+        temp[center[1]-50:center[1]+50, center[0]-50:center[0]+50] = 255
+
+        masked = cv2.bitwise_and(img, img, mask=temp)
+
+        masked = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+        _ROI_thre, _ROI_bina = cv2.threshold(masked, 80, 255, cv2.THRESH_BINARY)
+        return _ROI_bina
+
+def drawOctagon(img, center, radius, RIO_ratio, rotate_Angle):  #Draw octagon in image image with diff angle
+    x = center[0]
+    y = center[1]
+    InitAngle = 0
+    locationX, LocationY = [ ((x - radius*RIO_ratio - x) * np.cos(np.radians(rotate_Angle)) + (y - y) * np.sin(np.radians(rotate_Angle)) + x), \
+                            ((y - y) * np.cos(np.radians(rotate_Angle)) - (x - radius*RIO_ratio - x) * np.sin(np.radians(rotate_Angle)) + y)]
+
+    octagon_piont = [[int(x), int(y)], [int(x), int(y)], [int(x), int(y)],
+                     [int(x), int(y)],
+                     [int(x), int(y)], [int(x), int(y)], [int(x), int(y)],
+                     [int(x), int(y)]]
+                                                                # old version for locate eight points, but now use hexagon_piont way, use it to hexagon_point for  array frame.
+
+    for i in range(8):                                          # count eight points of hexagon after the init start_x,start_y changed by Mangle as LocationX/LocationY
+        octagon_piont[i] = [math.ceil((locationX - x) * np.cos(np.radians(InitAngle)) + (LocationY - y ) * np.sin(np.radians(InitAngle)) + x), \
+                            math.ceil((LocationY - y) * np.cos(np.radians(InitAngle)) - (locationX - x) * np.sin(np.radians(InitAngle)) + y)] # noticed the round way,the value would be lost 0 to 1
+        InitAngle += 45
+    octagon = np.array([[octagon_piont[0], octagon_piont[1], octagon_piont[2], octagon_piont[3], octagon_piont[4],
+                         octagon_piont[5], octagon_piont[6], octagon_piont[7]]], dtype=np.int32)
+    cv2.polylines(img, octagon, 1, (0, 255, 0))
+    return img
